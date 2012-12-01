0972272bf6af5af4cd255ce95ee3d39854d9f853
jay <shaoshijie830413@foxmail.com>
2012-12-01 16:43:03 +0800

    set up website

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArtflowBootstrap::Application.config.secret_token = '2a92e6af5c9f7c3d72c03053da000e3e37f25305dfcb0482ef0f63df7436350842143fbb297b886e1a395215db17f99cf37435db2b315e5d1ed59ee395b00cde'
